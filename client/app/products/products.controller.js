a1847d69efa8518d365e69ea4bbf18c84dbada81
Angel Diaz <angel.diaz@gmail.com>
2015-04-20 22:46:35 +0200

    feat(jade): new product

--- client/app/products/products.controller.js
+++ client/app/products/products.controller.js
@@ -1,6 +1,12 @@
 'use strict';
 
 angular.module('finanzieApp')
-  .controller('ProductsCtrl', function ($scope) {
-    $scope.message = 'Hello';
+  .controller('ProductsCtrl', function ($scope, userService) {
+
+    userService.adminPage();
+
+    $scope.showNew = function () {
+      $location.url('new-product');
+    };
+
   });
