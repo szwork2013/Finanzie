e8342f1bbbcf0bca03dd328754d0de055a3981bf
Angel Diaz <angel.diaz@gmail.com>
2015-04-21 20:34:49 +0200

    feat(jade): panel

--- client/app/admin/admin.controller.js
+++ client/app/admin/admin.controller.js
@@ -1,20 +1,8 @@
 'use strict';
 
 angular.module('finanzieApp')
-  .controller('AdminCtrl', function ($scope, $http, Auth, User, userService) {
+  .controller('AdminCtrl', function ($scope, $location, userService) {
 
     userService.adminPage();
 
-
-    // Use the User $resource to fetch all users
-    $scope.users = User.query();
-
-    $scope.delete = function(user) {
-      User.remove({ id: user._id });
-      angular.forEach($scope.users, function(u, i) {
-        if (u === user) {
-          $scope.users.splice(i, 1);
-        }
-      });
-    };
   });
